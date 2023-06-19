17620ae555cc029f049032bd3ded79edf8cc65ee
Anowar Hossen Farvez <farvez165@gmail.com>
2023-06-19 23:43:02 +0600

    path added for login, logout and dashboard

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
